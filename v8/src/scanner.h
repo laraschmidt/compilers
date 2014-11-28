db361109fa5b2b36c96f82277ceb0a2fbb868afd
lschmidt <lschmidt@cs.utexas.edu>
2014-11-28 16:27:52 -0600

    adding the scanner change to make it work in v8

--- v8/src/scanner.h
+++ v8/src/scanner.h
@@ -349,7 +349,7 @@ class Scanner {
   void Initialize(Utf16CharacterStream* source);
 
   void SetIso(Isolate* iso);
-  Isolate * iso = 0;
+  Isolate * iso;
 
   // Returns the next token and advances input.
   Token::Value Next();
